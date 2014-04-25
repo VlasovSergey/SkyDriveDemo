fcca65a83b301fb1d4a8b62dae96b5d8f9bdf15c
VlasovSergey <qlir@ya.ru>
2014-04-25 14:50:50 +0400

    fixes hide progress indicator on sign out

--- www/js/index.js
+++ www/js/index.js
@@ -5,5 +5,6 @@ var ctrl = ngOneDriveCtrl();
 ctrl.initialize();
 document.addEventListener('deviceready', function() {
     $('body').addClass(device.platform);
+    ProcessIndicator.hide();
     //ctrl.run();
 });
