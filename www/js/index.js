b3d6ef471ef4829678846ed8403716224bb7d6ba
VlasovSergey <qlir@ya.ru>
2014-04-28 11:41:40 +0400

    fixes progres-indicator hide after signout.

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,6 @@ var ctrl = ngOneDriveCtrl();
 ctrl.initialize();
 document.addEventListener('deviceready', function() {
     $('body').addClass(device.platform);
-    ProcessIndicator.hide();
+    ProgressIndicator.hide();
     //ctrl.run();
 });
